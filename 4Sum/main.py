aec00a10380126cc9f8d1420be0ee345d20d7003
mehkey <87057470+mehkey@users.noreply.github.com>
2023-05-26 23:58:55 -0400

    Update main.py

--- 4Sum/main.py
+++ 4Sum/main.py
@@ -38,45 +38,38 @@ def binary_search(arr, left,right,s):
       right -= 1
     else:
       left += 1
-    '''
-    mid = left + ((right-left) // 2)
-    
-    if arr[mid] == s:
-      return mid
 
-    if arr[mid] > s:
-      left = mid + 1
-    else:
-      right = mid - 1
-      
-    '''
 
   return -1
 
 def find_array_quadruplet(arr, s):
   
   arr.sort()
-  #sort by ascending
-  
+
   se = set(arr)
   
   for i in range(0,len(arr)-3):
     
     for j in range(i+1,len(arr)-2):
-      #twoSumHelper(arr, j + 1, s - arr[i] - arr[j]);
-      
+
       for k in range(j+1,len(arr)-1):
         
         total = arr[i] + arr[j] 
         
-        #if s - total in se:
-        #  return [arr[i], arr[j], arr[k], s-total]
+        while left < right:
+    
+          total = arr[left] + arr[right]
+          
+          if total == s:
+            return [arr[i], arr[j], arr[left],arr[right]]
+          
+          if total > s:
+            right -= 1
+          else:
+            left += 1
 
-        res =  binary_search(arr, k, len(arr)-1, s - total )
 
-        if res != -1:
-
-          return [arr[i], arr[j], res[0],res[1]]
+          
     
   return []
 
@@ -122,3 +115,18 @@ def find_array_quadruplet_up_to_k(arr, s, k, current, start):
 #print(find_array_quadruplet([2, 7, 4, 0, 9, 5, 1, 3],20))
 
 #print(binary_search([1,2,3,4,5,6],0,5,3))
+
+
+    '''
+
+    mid = left + ((right-left) // 2)
+    
+    if arr[mid] == s:
+      return mid
+
+    if arr[mid] > s:
+      left = mid + 1
+    else:
+      right = mid - 1
+
+    '''
